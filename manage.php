22b0ec4152f589a67911d80c1026007beee4f970
incrwd <incrwd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-05 17:11:15 +0000

    Version 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/incrwd/tags/1.2.3@527763 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- manage.php
+++ manage.php
@@ -1,34 +1,60 @@
 <?php
-if (isset($_POST['incrwd_site_id']) && isset($_POST['incrwd_secret_key'])) {
-  update_option('incrwd_site_id', $_POST['incrwd_site_id']);
-  update_option('incrwd_secret_key', $_POST['incrwd_secret_key']);
-  echo "<div><p><strong>Your settings have been changed.</strong></p></div>";
-}
+define("TRUE_STRING", "true");
+define("FALSE_STRING", "false");
 
-$incrwd_site_id = get_option('incrwd_site_id');
-$incrwd_secret_key = get_option('incrwd_secret_key');
+$in_excerpt = get_option('add_share_widget_excerpt') == TRUE_STRING;
+$in_content = get_option('add_share_widget_content') == TRUE_STRING;
+$in_content_top = get_option('add_share_widget_content_top') == TRUE_STRING;
 ?>
 <div id="incrwd-options" class="incrwd-settings">
-   <h2>Incrwd Settings</h2>
-   <form method="POST">
-       <table class="form-table">
-            <tr>
-                <th scope="row" valign="top">Incrwd Site ID</th>
-                <td>
-                    <input type="text" name="incrwd_site_id" value="<?php echo esc_attr($incrwd_site_id); ?>" tabindex="2">
-                </td>
-            </tr>
-            <tr>
-                <th scope="row" valign="top">Incrwd Secret Key</th>
-                <td>
-                    <input type="text" name="incrwd_secret_key" value="<?php echo esc_attr($incrwd_secret_key); ?>" tabindex="2">
-                </td>
-            </tr>
-       </table>
-        <p class="submit" style="text-align: left">
-            <input name="submit" type="submit" value="Save" class="button-primary button" tabindex="4">
-        </p>
-   </form>
+   <h2 style="font-family: Helvetica Neue, Helvetica, Arial, Sans-Serif;">Incrwd Settings</h2>
+   <h3 style="margin-left:15px;">Engagement Rewards Widget: <span style='color:#7EB54A;'>Active</span></h3>
+   <?php
+   
+  $post_excerpt = $_POST['add_share_widget_excerpt'] == TRUE_STRING;
+  $post_content = $_POST['add_share_widget_content'] == TRUE_STRING;
+  $post_content_top = $_POST['add_share_widget_content_top'] == TRUE_STRING;
+
+  if ($_POST) {
+    $currently_activated = $post_excerpt || $post_content || $post_content_top;   
+  }
+  else {
+    $currently_activated = $in_excerpt || $in_content || $in_content_top;
+  }
+
+  if($_POST) {
+    $in_excerpt = $post_excerpt;
+    update_option('add_share_widget_excerpt', $in_excerpt ? TRUE_STRING : FALSE_STRING);
+  
+    $in_content = $post_content;
+    update_option('add_share_widget_content', $in_content ? TRUE_STRING : FALSE_STRING);
+     
+    $in_content_top = $post_content_top;
+    update_option('add_share_widget_content_top', $in_content_top ? TRUE_STRING : FALSE_STRING);
+  }
+  
+  ?>
+   <div id="left">
+    <form method="POST">
+      <?
+         if ($currently_activated) {
+           echo "<h3 style='margin-left:15px;'><strong>Share Widget Settings: <span style='color:#7EB54A;'>Active</strong></span></h3>";
+         }
+         else {
+           echo "<h3 style='margin-left:15px;'><strong>Share Widget Settings: <span style='color: red; text-size: 18px;'>Deactivated</strong></span></h3>";
+         }
+      ?>
+      <input type="checkbox" name="add_share_widget_excerpt" style="margin-left:30px;" value="true" tabindex="3" <?php if ($in_excerpt) echo "checked"; ?>/> Add Share Widget to Post Listing (Main Page)<br/>
+      <input type="checkbox" name="add_share_widget_content_top" style="margin-left:30px;" value="true" tabindex="4" <?php if ($in_content_top) echo "checked"; ?>/> Add Share Widget to Top of Post<br/>
+      <input type="checkbox" name="add_share_widget_content" style="margin-left:30px;" value="true" tabindex="5" <?php if ($in_content) echo "checked"; ?>/> Add Share Widget to Bottom of Post
+      <p class="submit" style="text-align: left">
+      <input name="submit" style="margin-left:30px;" type="submit" value="Save" class="button-primary button" tabindex="6" />
+     </p>
+    </form>
+   </div>
 </div>
-<?php
-?>
+<hr />
+<div id="incrwd-dashboard">
+   <h2>Incrwd Widget Dashboard</h2>
+   <p style="margin-left:15px;">Sign in to the <a href="http://www.myincrwd.com/signup/login/?next=/signup/analytics_dashboard/">Incrwd Dashboard</a> to see analytics and customize your rewards... for free!</p>
+</div>
